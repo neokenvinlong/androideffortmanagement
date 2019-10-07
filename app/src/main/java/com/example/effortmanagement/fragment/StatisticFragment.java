36a6df932d0bac6c87acaaa2409b8699c061d129
effortmanagement <winsupersentai@gmail.com>
2019-10-07 23:17:21 +0700

    login and fix tab change

--- app/src/main/java/com/example/effortmanagement/fragment/StatisticFragment.java
+++ app/src/main/java/com/example/effortmanagement/fragment/StatisticFragment.java
@@ -41,7 +41,7 @@ public class StatisticFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_statistic, container, false);
+            View view = inflater.inflate(R.layout.fragment_statistic, container, false);
 
 
 //        Pie pie = AnyChart.pie();
--- app/src/main/java/com/example/effortmanagement/fragment/TaskListFragment.java
+++ app/src/main/java/com/example/effortmanagement/fragment/TaskListFragment.java
@@ -3,13 +3,30 @@ package com.example.effortmanagement.fragment;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ExpandableListView;
+import android.widget.SearchView;
+import android.widget.Toast;
 
 import com.example.effortmanagement.R;
+import com.example.effortmanagement.adapter.ExpandableListAdapter;
+import com.example.effortmanagement.adapter.ProjectAdapter;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 
 /**
@@ -17,17 +34,194 @@ import com.example.effortmanagement.R;
  */
 public class TaskListFragment extends Fragment {
 
+   // ///////////////
+    SearchView searchView;
+    ProjectAdapter projectAdapter;
+    List<ProjectItem> mData;
+    SearchView.OnQueryTextListener onQueryTextListener;
+    ////////////////////////////////
+
+    private ExpandableListAdapter listAdapter;
+    private ExpandableListView expListView;
+    private List<String> listDataHeader;
+    private HashMap<String, List<String>> listDataChild;
+    private Button btnAddTask;
+
+
 
     public TaskListFragment() {
         // Required empty public constructor
     }
 
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        setHasOptionsMenu(true);
+        super.onCreate(savedInstanceState);
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        View view;
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_task_list, container, false);
+        view = inflater.inflate(R.layout.fragment_task_list, container, false);
+
+        // get the listview
+        expListView = (ExpandableListView) view.findViewById(R.id.lvExp);
+
+        // get the button add task
+        btnAddTask = view.findViewById(R.id.btnAddTask);
+
+        // preparing list data
+        prepareListData();
+
+        listAdapter = new ExpandableListAdapter(requireContext(), listDataHeader, listDataChild);
+
+        // setting list adapter
+        expListView.setAdapter(listAdapter);
+
+        // Listview Group click listener
+        expListView.setOnGroupClickListener(new ExpandableListView.OnGroupClickListener() {
+
+            @Override
+            public boolean onGroupClick(ExpandableListView parent, View v,
+                                        int groupPosition, long id) {
+                // Toast.makeText(getApplicationContext(),
+                // "Group Clicked " + listDataHeader.get(groupPosition),
+                // Toast.LENGTH_SHORT).show();
+                return false;
+            }
+        });
+
+        // Listview Group expanded listener
+        expListView.setOnGroupExpandListener(new ExpandableListView.OnGroupExpandListener() {
+
+            @Override
+            public void onGroupExpand(int groupPosition) {
+                Toast.makeText(getContext(),
+                        listDataHeader.get(groupPosition) + " Expanded",
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        // Listview Group collasped listener
+        expListView.setOnGroupCollapseListener(new ExpandableListView.OnGroupCollapseListener() {
+
+            @Override
+            public void onGroupCollapse(int groupPosition) {
+                Toast.makeText(getContext(),
+                        listDataHeader.get(groupPosition) + " Collapsed",
+                        Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+        // Listview on child click listener
+        expListView.setOnChildClickListener(new ExpandableListView.OnChildClickListener() {
+
+            @Override
+            public boolean onChildClick(ExpandableListView parent, View v,
+                                        int groupPosition, int childPosition, long id) {
+                // TODO Auto-generated method stub
+                Toast.makeText(
+                        getContext(),
+                        listDataHeader.get(groupPosition)
+                                + " : "
+                                + listDataChild.get(
+                                listDataHeader.get(groupPosition)).get(
+                                childPosition), Toast.LENGTH_SHORT)
+                        .show();
+                return false;
+            }
+        });
+
+
+        Toolbar toolbar = view.findViewById(R.id.toolbar);
+        ((AppCompatActivity)getActivity()).setSupportActionBar(toolbar);
+
+        return view;
     }
 
+    /*
+     * Preparing the list data
+     */
+    private void prepareListData() {
+        listDataHeader = new ArrayList<String>();
+        listDataChild = new HashMap<String, List<String>>();
+
+        // Adding child data
+        listDataHeader.add("Project 1");
+        listDataHeader.add("Project 2");
+        listDataHeader.add("Project 3");
+
+        // Adding child data
+        List<String> project1 = new ArrayList<String>();
+        project1.add("Task 1 1");
+        project1.add("Task 1 2");
+        project1.add("Task 1 3");
+        project1.add("Task 1 4");
+        project1.add("Task 1 5");
+        project1.add("Task 1 6");
+
+        List<String> project2 = new ArrayList<String>();
+        project2.add("Task 2 1");
+        project2.add("Task 2 2");
+        project2.add("Task 2 3");
+        project2.add("Task 2 3");
+        project2.add("Task 2 4");
+        project2.add("Task 2 5");
+
+        List<String> project3 = new ArrayList<String>();
+        project3.add("Task 3 1");
+        project3.add("Task 3 2");
+        project3.add("Task 3 3");
+        project3.add("Task 3 4");
+        project3.add("Task 3 5");
+        project3.add("Task 3 6");
+
+        listDataChild.put(listDataHeader.get(0), project1); // Header, Child data
+        listDataChild.put(listDataHeader.get(1), project2);
+        listDataChild.put(listDataHeader.get(2), project3);
+    }
+
+
+    //////////////////////////////////////////////
+    @Override
+    public void onCreateOptionsMenu(@NonNull Menu menu, @NonNull MenuInflater inflater) {
+        //getMenuInflater().inflate(R.menu.toolbar_menu,menu);
+        inflater.inflate(R.menu.toolbar_menu, menu);
+        MenuItem menuItem = menu.findItem(R.id.action_search);
+        //SearchManager searchManager = (SearchManager) getActivity().getSystemService(Context.SEARCH_SERVICE);
+        if (menuItem != null) {
+            searchView = (SearchView) menuItem.getActionView();
+        }
+        if (menuItem != null) {
+            //searchView.setSearchableInfo(searchManager.getSearchableInfo(getActivity().getComponentName()));
+            onQueryTextListener = new SearchView.OnQueryTextListener() {
+                @Override
+                public boolean onQueryTextSubmit(String s) {
+                    return false;
+                }
+
+                @Override
+                public boolean onQueryTextChange(String newText) {
+                    String userInput = newText.toLowerCase();
+                    List<ProjectItem> projectItemList = new ArrayList<>();
+                    for (ProjectItem projectItem : mData) {
+                        if (projectItem.getProjectName().toLowerCase().contains(userInput)) {
+                            projectItemList.add(projectItem);
+                        } else if (projectItem.getProjectStatus().toLowerCase().contains(userInput)) {
+                            projectItemList.add(projectItem);
+                        }
+                    }
+                    projectAdapter.updateList(projectItemList);
+//                    Fragment fragment = getFragmentManager().findFragmentById(R.id.project_list_fragment);
+//                    fragment.getView().setBackgroundColor(R.drawable.bg);
+                    return true;
+                }
+            };
+            searchView.setOnQueryTextListener(onQueryTextListener);
+        }
+        super.onCreateOptionsMenu(menu, inflater);
+    }
 }
